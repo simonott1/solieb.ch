2da23f3621b55df87a97b41ab9e9d5bd78f50a3d
simonott1 <simon.ott1@gmx.ch>
2024-04-12 18:49:19 +0200

    change design and content to soliieb

---
+++ scripts/gallery_script.js
@@ -0,0 +1,30 @@
+let i = 0; // current slide
+let j = 4; // total slides
+
+const images = document.querySelectorAll(".image-container img");
+
+function next(){
+    document.getElementById("content" + (i+1)).classList.remove("active");
+    i = ( j + i + 1) % j;
+    document.getElementById("content" + (i+1)).classList.add("active");
+    indicator( i+ 1 );
+}
+
+function prev(){
+    document.getElementById("content" + (i+1)).classList.remove("active");
+    i = (j + i - 1) % j;
+    document.getElementById("content" + (i+1)).classList.add("active");
+    indicator(i+1);
+}
+
+
+ document.onkeydown = function(event) {
+         switch (event.keyCode) {
+            case 37:
+               prev();
+            break;
+            case 39:
+               next()
+            break;
+         }
+      };
